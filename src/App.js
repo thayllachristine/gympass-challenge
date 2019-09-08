8bc134d6a0b09d6928100e7b26d0dbc88e854b3c
Thaylla Christine <thayllachristineo@gmail.com>
2019-09-08 12:44:14 -0300

    feat: config app routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './pages/home'
+import RepoResult from './pages/repoResult'
+import CommitsResult from './pages/commitsResult'
+import NotFound from './pages/notFound'
+
+import GlobalStyle from './helpers/GlobalStyle'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <GlobalStyle />
+      <Route path='/' exact component={Home} />
+      <Route path='/:user/repositories' component={RepoResult} />
+      <Route path='/:user/:repo/commits' component={CommitsResult} />
+      <Route path='/error' component={NotFound} />
+    </Router>
+  )
 }
 
 export default App;
